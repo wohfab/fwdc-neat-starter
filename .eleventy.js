794cfca800ed1d722b95e3926d54d16c97d6107f
wohfab <wohfab@gmail.com>
2021-02-15 20:39:06 +0100

    spotify shortcode

--- .eleventy.js
+++ .eleventy.js
@@ -6,6 +6,10 @@ const markdownIt = require('markdown-it');
 const Image = require('@11ty/eleventy-img');
 const rssPlugin = require('@11ty/eleventy-plugin-rss');
 const pluginSass = require('eleventy-plugin-sass');
+const localImages = require('eleventy-plugin-local-images');
+const svgContents = require('eleventy-plugin-svg-contents');
+const fetch = require('node-fetch');
+const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
 
 
 // Image shortcode
@@ -38,6 +42,63 @@ async function imageShortcode(src, alt, sizes) {
   return Image.generateHTML(metadata, imageAttributes);
 }
 
+async function getSpotifyCode(
+  id,
+  classes = '',
+  createLink = true,
+  showSpotifyLogo = true,
+  removeBg = true,
+  removeFg = true,
+  fileType = 'svg',
+  bg = 'FFFFFF',
+  color = 'black',
+  size = 1000,
+  mediaType = 'track',
+) {
+  // Base URL for the Spotify Scannable Code 'API'
+  const base = 'https://scannables.scdn.co/uri/plain';
+  // Concatinating the values to get fetch URL
+  spotifyUri = ['spotify', mediaType, id].join(':');
+  url = [base, fileType, bg, color, size, spotifyUri].join('/');
+
+  // Fetching the HTML Code
+  // console.log(url);
+  const html = await fetch(url).then((response) => response.text());
+
+  var output = html;
+
+  // Remove Background
+  if (removeBg) {
+      output = output.replace(/<rect x="0" y="0".*/g, '');
+  }
+  // Remove Foreground
+  if (removeFg) {
+      output = output.replace(/fill="#000000"/g, '');
+  }
+
+  // Add Classes
+  output = output.replace('<svg', '<svg class="' + classes + '"');
+
+  // Remove Spotify Logo and narrow the viewport
+  if (!showSpotifyLogo) {
+    output = output.replace(/<g transform.*/g, '');
+    output = output.replace(
+      /width.*viewBox=".*"/g,
+      'viewBox="50 0 400 100"',
+    );
+  }
+
+  // Create surrounding <a></a> tags with link to Media
+  if (createLink) {
+    link = ['<a href="https://open.spotify.com', mediaType, id].join('/');
+    output = link + '" rel="noopener" target="_blank">' + output + '</a>';
+  }
+
+  // Output
+  console.log(output);
+  return output.trim().replace(/\r?\n|\r/g, '');
+}
+
 // Eleventy settings
 module.exports = function (config) {
   // Layout aliases
@@ -47,7 +108,7 @@ module.exports = function (config) {
 
   // Disable extended console output
   config.setQuietMode(true);
-  
+
   // Merge data instead of overriding
   config.setDataDeepMerge(true);
 
@@ -71,13 +132,10 @@ module.exports = function (config) {
   config.addPassthroughCopy({
     // Styles
     './_tmp/static/css': './static/css',
-
     // Configs
     './src/admin/config.yml': './admin/config.yml',
-
     // Javascript
     './node_modules/alpinejs/dist/alpine.js': './static/js/alpine.js',
-
     // Images
     './src/static/img': './static/img',
     './src/favicon.ico': './favicon.ico',
@@ -106,16 +164,27 @@ module.exports = function (config) {
   config.addLiquidShortcode('image', imageShortcode);
   config.addJavaScriptFunction('image', imageShortcode);
 
+  config.addNunjucksAsyncShortcode('getSpotifyCode', getSpotifyCode);
+  config.addLiquidShortcode('getSpotifyCode', getSpotifyCode);
+  config.addJavaScriptFunction('getSpotifyCode', getSpotifyCode);
+
   // Plugins
   config.addPlugin(rssPlugin);
   config.addPlugin(pluginSass);
+  config.addPlugin(localImages, {
+    distPath: '_site',
+    assetPath: '/static/img',
+    selector: 'img',
+  });
+  config.addPlugin(svgContents);
+  config.addPlugin(syntaxHighlight);
 
   // Return
   return {
     // Directory settings
     dir: {
       input: 'src',
-      outpur: '_site',
+      output: '_site',
 
       // Relative to above-set input folder
       includes: '_includes',
--- src/index.md
+++ src/index.md
@@ -1,14 +1,40 @@
 ---
 layout: default
-title: Welcome to the Jungle.
+title: FWDC NEAT Starter
 path: home
-quicklinks: true
+prism: true
 ---
+## Functionality
+### Shortcodes
 
-Sit cupidatat irure elit ipsum aliqua minim veniam veniam sunt et dolore. Anim pariatur sit do mollit ipsum fugiat Lorem occaecat **laboris cillum elit** commodo. Et ad ipsum ea commodo esse. [Adipisicing officia quis](https://google.com) nulla nostrud. Proident sunt ut tempor eiusmod et irure. **Sint quis cillum nulla** voluptate id officia proident sit ad proident. _Non magna eu deserunt_ qui sunt magna elit ea adipisicing enim aliquip proident dolor esse.
+#### getSpotifyCode
 
-Fugiat laboris mollit reprehenderit aliquip tempor aliquip veniam exercitation enim esse ea. Dolor ipsum eiusmod dolor amet ea. Qui occaecat consectetur ad aute aliquip aliqua mollit. Nisi cillum ullamco ullamco id aliquip aliqua elit nostrud aute excepteur adipisicing. Duis ipsum in consectetur do in amet culpa eu pariatur est reprehenderit. Aliqua commodo enim incididunt velit tempor.
+Embeds a Spotify Share Code by *Spotify URI*.
 
-Consectetur non et elit et excepteur. Cupidatat nulla nisi ex amet ut laboris est occaecat proident tempor exercitation nostrud eiusmod exercitation. Laborum deserunt aute tempor incididunt aute veniam est.
+``` js
+{{ "{% getSpotifyCode id, classes, createLink, showSpotifyLogo, fileType, bg, color, size, mediaType %}" }}
+```
 
-Adipisicing occaecat officia dolor consectetur qui aliquip voluptate. Consequat reprehenderit exercitation aliqua reprehenderit do nisi proident sunt Lorem ea anim dolor amet dolore. Deserunt tempor est nisi qui nisi esse pariatur cupidatat aliqua aliquip. Nostrud sit voluptate reprehenderit duis veniam. Irure Lorem laboris aliqua est mollit non esse mollit nulla.
+{% getSpotifyCode '4x5wzixdnGpq36kopc3oju', 'mx-auto h-auto w-64 fill-current text-pink-600' %}
+
+Options, their default values, and their types:
+
+``` js
+id // Str, Spotify URI, required
+classes = '' // Str, adds classes to output
+createLink = true // Bool, add <a> to output
+showSpotifyLogo = true // Bool, shows/hides Spotify Logo
+removeBg = true // Bool, removes Background color for transparency
+removeFg = true // Bool, Removes Foreground color to use own
+fileType = 'svg' // Str, Output format (svg or png)
+bg = 'FFFFFF' // Str, HEX-Code for Background color
+color = 'black' // Str, HTML-Color (e.g. white, green) for foreground
+size = 1000 // Int, Size in px
+mediaType = 'track' // Str, Media Type (e.g. playlist)
+```
+
+Example:
+
+
+
+### Quicklinks
